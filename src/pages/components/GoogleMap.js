2bf466b63d7f2510338874dc864300a3d4f6fc70
dendenxu <56295612@qq.com>
2021-06-28 09:40:19 +0800

    feat: data acquired

--- src/pages/components/GoogleMap.js
+++ src/pages/components/GoogleMap.js
@@ -14,40 +14,18 @@ const loadAMap = async () => {
   });
 };
 
-// import { AMapLoader, AMapUILoader, LocaLoader } from 'amap-js';
-
-// const mapLoader = new AMapLoader({
-//   key: 'c0e09fc65ece185ee473aab8c33e1c6e', // 申请好的Web端开发者Key，首次调用 load 时必填
-//   version: '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-//   plugins: [],
-// });
-
-// const locaLoader = new LocaLoader({
-//   key: 'c0e09fc65ece185ee473aab8c33e1c6e', // 申请好的Web端开发者Key，首次调用 load 时必填
-//   version: '2.0.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-// });
-
 export default function SimpleMap() {
   useEffect(async () => {
     await loadAMap();
-
-    // await mapLoader.load();
-    // await locaLoader.load();
-    // const AMap = await mapLoader.load();
-    // const Loca = await locaLoader.load();
     const { Loca, AMap } = window;
     const map = new AMap.Map('map', {
       zoom: 11.2,
-      center: [116.352734, 39.8447],
+      center: [119.9, 30.1],
       // showLabel: false,
       viewMode: '3D',
       mapStyle: 'amap://styles/dark',
-      pitch: 50,
+      pitch: 30,
     });
-
-    // map.AmbientLight = new AMap.Lights.AmbientLight([1, 1, 1], 0.4);
-
-    // map.AmbientLight
 
     console.log('AmbientLight: ', map.AmbientLight);
 
@@ -55,34 +33,16 @@ export default function SimpleMap() {
       map,
     });
 
-    loca.ambLight = {
-      intensity: 2.2,
-      color: '#babedc',
-    };
-    loca.dirLight = {
-      intensity: 0.46,
-      color: '#d4d4d4',
-      target: [0, 0, 0],
-      position: [0, -1, 1],
-    };
-    loca.pointLight = {
-      color: 'rgb(15,19,40)',
-      position: [121.5043258, 31.2392241, 2600],
-      intensity: 25,
-      // 距离表示从光源到光照强度为 0 的位置，0 就是光不会消失。
-      distance: 3900,
-    };
-
     console.log(map);
     console.log(AMap);
 
     const geo = new Loca.GeoJSONSource({
-      url: 'https://a.amap.com/Loca/static/loca-v2/demos/mock_data/bj_bus.json',
+      url: '/api/message/route?fromMills=1624839660000&toMills=1624846680000',
     });
 
     const layer = new Loca.PulseLineLayer({
-      // loca,
-      zIndex: 10,
+      loca,
+      zIndex: 100,
       opacity: 1,
       visible: true,
       zooms: [2, 22],
@@ -100,18 +60,19 @@ export default function SimpleMap() {
     ];
 
     console.log('geo', geo);
-    layer.setSource(geo);
-    layer.setStyle({
+    layer.setSource(geo, {
+      // altitude: (index, feature) => feature.properties.type * 100,
       altitude: 0,
       lineWidth: 2,
+      color: (index, feature) => headColors[feature.properties.type],
       // 脉冲头颜色
-      headColor: (_, feature) => headColors[feature.properties.type - 1],
+      headColor: (index, feature) => headColors[feature.properties.type],
       // 脉冲尾颜色
       trailColor: 'rgba(128, 128, 128, 0.5)',
       // 脉冲长度，0.25 表示一段脉冲占整条路的 1/4
-      interval: 0.25,
+      interval: 1,
       // 脉冲线的速度，几秒钟跑完整段路
-      duration: 5000,
+      duration: 10000,
     });
     loca.add(layer);
     loca.animate.start();
